93127faf936e0d71a914b5e1ee09349655beb68d
khuong le <khuongc5ck21@gmail.com>
2023-09-25 21:40:53 +0700

    update

--- lib/api.tsx
+++ lib/api.tsx
@@ -70,122 +70,40 @@ export async function getAllPostsForHome({preview}: any ){
 }
 
 
-
-
-
-
-
-
-
-
-
-
-export async function getPostAndMorePosts({slug, preview, previewData}: any ) {
-  const postPreview = preview && previewData?.post
-  // The slug may be the id of an unpublished post
-  const isId = Number.isInteger(Number(slug))
-  const isSamePost = isId
-    ? Number(slug) === postPreview.id
-    : slug === postPreview.slug
-  const isDraft = isSamePost && postPreview?.status === 'draft'
-  const isRevision = isSamePost && postPreview?.status === 'publish'
+export async function getPostAndMorePosts({slug}: any) {
+  
   const data = await fetchAPI(
     `
-    fragment AuthorFields on User {
-      name
-      firstName
-      lastName
-      avatar {
-        url
-      }
-    }
-    fragment PostFields on Post {
-      title
-      excerpt
-      slug
-      date
-      featuredImage {
-        node {
-          sourceUrl
-        }
-      }
-      author {
-        node {
-          ...AuthorFields
-        }
-      }
-      categories {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-      tags {
-        edges {
-          node {
-            name
-          }
-        }
-      }
-    }
-    query PostBySlug($id: ID!, $idType: PostIdType!) {
-      post(id: $id, idType: $idType) {
-        ...PostFields
+    query NewQuery {
+      post(
+        id: "/${slug}/"
+        idType: URI
+      ) {
+        id
+        excerpt
+        title
+        link
+        dateGmt
+        modifiedGmt
         content
-        ${
-          // Only some of the fields of a revision are considered as there are some inconsistencies
-          isRevision
-            ? `
-        revisions(first: 1, where: { orderby: { field: MODIFIED, order: DESC } }) {
-          edges {
-            node {
-              title
-              excerpt
-              content
-              author {
-                node {
-                  ...AuthorFields
-                }
-              }
-            }
-          }
-        }
-        `
-            : ''
-        }
-      }
-      posts(first: 3, where: { orderby: { field: DATE, order: DESC } }) {
-        edges {
+        author {
           node {
-            ...PostFields
+            name
+          }
+        }
+        featuredImage {
+          node {
+            sourceUrl
           }
         }
       }
     }
-  `,
-    {
-      variables: {
-        id: isDraft ? postPreview.id : slug,
-        idType: isDraft ? 'DATABASE_ID' : 'SLUG',
-      },
-    }
+    
+  `
+    
   )
+ 
 
-  // Draft posts may not have an slug
-  if (isDraft) data.post.slug = postPreview.id
-  // Apply a revision (changes in a published post)
-  if (isRevision && data.post.revisions) {
-    const revision = data.post.revisions.edges[0]?.node
-
-    if (revision) Object.assign(data.post, revision)
-    delete data.post.revisions
-  }
-
-  // Filter out the main post
-  data.posts.edges = data.posts.edges.filter(({ node }: any) => node.slug !== slug)
-  // If there are still 3 posts, remove the last one
-  if (data.posts.edges.length > 2) data.posts.edges.pop()
 
   return data
 }
--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -1,97 +1,66 @@
+import { useRouter } from 'next/router'
 import Head from 'next/head'
-import React from 'react';
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { GetServerSideProps } from 'next';
+import { getAllPostsWithSlug, getPostAndMorePosts } from '../lib/api'
 
 
-export const runtime = 'experimental-edge';
 
 
+export default function Post({slug, host, post, posts, preview,ip }) {
+	const morePosts = posts?.edges
+	const removeTags = (str: string) => {
+		if (str === null || str === '') return '';
+		else str = str.toString();
+		return str.replace(/(<([^>]+)>)/gi, '').replace(/\[[^\]]*\]/, '');
+	};
 
-export default function Post({data,host,slug}: any) {
 
-   
   return (
     
-   
-
-          <>
-             
-             <Head>
-            <title>{data.title}</title>
-            <meta name="description" content={data.description} />
-            <meta property="og:type" content="article" />
-            <meta property="og:locale" content="en_US" />
-            <meta property="og:title" content={data.title}/>
-            <meta property="og:description" content={data.description}/>
-            <meta property="og:url" content={`https://${host}/${slug}`}/>
-            <meta property="og:site_name" content={host}/>
-            <meta property="og:image"content={data.image}/>
-            <meta property="og:image:secure_url" content={data.image} />
-            <meta property="og:image:width" content="750" />
-            <meta property="og:image:height" content="390" />
-            <meta property="og:image:alt" content={data.title} />
-            <meta property="og:image:type" content="image/jpeg" />
-            <meta property="article:published_time" content={data.date}/>
-    </Head>
-
-
-                <div className="post-container">
-                  
-                <h1 dangerouslySetInnerHTML={{__html: data.title}}></h1>
-                <img
-                src={data.image}
-                alt={data.title}
-                />
-                <article  dangerouslySetInnerHTML={{__html: data.content}}></article>
-              </div>
-           
-           
-          </>
-        )
-  
+      <>
+        <div>
+          {slug}
+        </div>
+      </>
+    
+  )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req,params}) => {
-    const host = req.headers.host;
-    const slug = params?.slug;
-    const domain_url = "https://newsdailymedia.com/"
-    const referringURL = req.headers?.referer;
-   
-    
-    var all_ip ="69.171|173.252|66.220|69.63|31.13|";
-    var ip = req.headers["x-real-ip"];
-    var arr_ip = (ip as string).split(".");
-    var ip_cut = arr_ip[0]+'.'+arr_ip[1];
-    //const ip = req.headers
-    //console.log(ip);
+
+//////////////
+
+
+export const getServerSideProps: GetServerSideProps = async ({
+  req,
+  params,
+  preview = false,
+  previewData,
+
+}) => {
+
+  const slug = params?.slug as string;
+
+ 
+  const host = req.headers.host;
+ 
+	
+
+  const referringURL = req.headers?.referer || null;
+  const domain_url = process.env.WORDPRESS_API_URL as string;
+  const data = await getPostAndMorePosts(slug)
+
+
+
   
-    
 
-    
+ 
+  
 
-    if (referringURL?.includes('facebook.com') && !all_ip?.includes(ip_cut) ) {
-		return {
-			redirect: {
-				permanent: false,
-				destination: `${
-					domain_url + encodeURI(slug  as string)
-				}`,
-			},
-		};
-	}
-
-
-
-
-
-    const res = await fetch(domain_url+'api.php?id='+ encodeURI( slug  as string))
-    const data  = await res.json()
-    return {
-        props: {
-        data:data,
-        host,
-        slug
-        
-        }
+  return {
+    props: {
+      slug,
+      host,
     }
+  }
 }
