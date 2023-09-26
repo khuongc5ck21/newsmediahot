9abbe81579f4db1d5bee2927c5d8dc63a01d9b30
khuong le <khuongc5ck21@gmail.com>
2023-09-26 21:52:56 +0700

    update

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,13 +1,7 @@
 import Head from 'next/head'
-import { GetStaticProps } from 'next'
 import { GetServerSideProps } from 'next';
-import Container from '../components/container'
-import MoreStories from '../components/more-stories'
-import HeroPost from '../components/hero-post'
-import Intro from '../components/intro'
-import Layout from '../components/layout'
 import { getAllPostsForHome } from '../lib/api'
-import { CMS_NAME } from '../lib/constants'
+
 
 export default function Index({ allPosts, preview }) {
  
