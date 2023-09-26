c45b96cef95ef32edc7d54724fe660a134440a3d
khuong le <khuongc5ck21@gmail.com>
2023-09-26 21:57:45 +0700

    update

--- pages/post.tsx
+++ pages/post.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import { GetServerSideProps } from 'next';
 import { getAllPostsForHome } from '../lib/api'
 
-
+export const runtime = 'experimental-edge';
 export default function Index({ allPosts, preview }:any) {
  
 console.log(allPosts)
