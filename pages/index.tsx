ad95be1e6d4cfe5de2209df185fc6fb09422982b
khuongc5ck21 <118723465+khuongc5ck21@users.noreply.github.com>
2024-11-21 20:16:01 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+compatibility_flags = [ "nodejs_compat" ]
+
 
 export default function Home() {
   return (
